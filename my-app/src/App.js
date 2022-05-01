2e5b0acb285893f390cc85a4eb7de874635bf63a
MiroslavIvanov <miro.marvel@prtonmail.com>
2022-05-01 20:28:14 +0300

    Add header, footer and home pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Home from "./components/Home.js";
+
 function App() {
-    return ( 
-    <div className="App">
+    return (
+        <div className="App">
+            {/* <Header /> */}
+
+            <main className="wrapper">
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+
+            </main>
+
+            {/* <Footer /> */}
         </div>
     );
 }
