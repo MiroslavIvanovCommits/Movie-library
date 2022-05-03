207b2841ce9b133630f980296da5840539aa1718
MiroslavIvanov <miro.marvel@prtonmail.com>
2022-05-03 13:50:53 +0300

    Split navbar for users and guests

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
 import "./Header.css";
 
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext.js";
+
 const Header = () => {
+    const { user } = useContext(AuthContext);
+
+    let guestNavigation = (
+        <>
+            <Link to="/login" className="button">Login</Link>
+            <Link to="/register" className="button">Register</Link>
+        </>
+    );
+    let userNavigation = (
+        <Link to="/logout" className="button">Logout</Link>
+    );
+
     return (
         <header>
             <div className="topnav">
                 <nav className="navigation">
-                        <Link to="/">My Movie Collection</Link>
-                        <Link to="/search">Search</Link>
-                        <Link to="/login" className="button">Login</Link>
-                        <Link to="/register" className="button">Register</Link>
-                        <Link to="/logout" className="button">Logout</Link>
+                    <Link to="/">My Movie Collection</Link>
+                    <Link to="/search">Search</Link>
+                    {user.email
+                        ? userNavigation
+                        : guestNavigation
+                    }
                     <div className="search-container">
                         <form action="/action_page.php">
                             <input type="text" placeholder="Search by movie title..." name="search" />
