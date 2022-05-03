aa0ce035e5b067c5189238f2fa5f7e6ff91b1fee
MiroslavIvanov <miro.marvel@prtonmail.com>
2022-05-03 14:00:44 +0300

    Refactor code by adding AuthProvider and cleaning App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 
 import { Routes, Route } from "react-router-dom";
-import { AuthContext } from "./contexts/AuthContext.js";
-import useLocalStorage from "./hooks/useLocalStorage.js";
+import { AuthProvider } from "./contexts/AuthContext.js";
 
 import Header from "./components/Header/Header.js";
 import Footer from "./components/Footer/Footer.js";
@@ -13,25 +12,9 @@ import Login from "./components/Login/Login.js";
 import Register from "./components/Register/Register.js";
 import Logout from "./components/Logout/Logout.js";
 
-const initialAuthState = {
-    accessToken: "",
-    emal: "",
-    _id: ""
-};
-
 function App() {
-    const [user, setUser] = useLocalStorage("user", initialAuthState);
-
-    const login = (authData) => {
-        setUser(authData);
-    };
-
-    const logout = () => {
-        setUser(initialAuthState);
-    };
-
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthProvider>
             <div className="App">
                 <Header />
 
@@ -51,7 +34,7 @@ function App() {
 
                 <Footer />
             </div>
-        </AuthContext.Provider>
+        </AuthProvider>
     );
 }
 
